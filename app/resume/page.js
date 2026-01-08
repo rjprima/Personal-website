5b3146d541f4f9651dc8260f90a81a135fdab823
rjprima <rileyprimavera5@gmail.com>
2026-01-08 03:26:31 -0800

    minor changes

--- app/resume/page.js
+++ app/resume/page.js
@@ -2,7 +2,7 @@ export default function resume() {
     return (
         <div className="centered">
             <p className="fixed top-0 left-[25px] text-[2.7rem] z-11 invisible md:visible text-(--colormode1)"><b>Resume</b></p>
-            <iframe src="/Riley_Primavera.pdf" width="650" height="655"></iframe>
+            <iframe src="/Riley Primavera - resume.pdf" width="650" height="655"></iframe>
         </div>
     );
 }
